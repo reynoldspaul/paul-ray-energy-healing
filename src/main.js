46a81ab3e7e283d7db322f8760137bbd6bf32d73
Paul Reynolds <paul.reynolds@hotmail.co.uk>
2025-06-12 14:42:32 +0100

    404

--- src/main.js
+++ src/main.js
@@ -9,9 +9,18 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// gh pages workaround for loading page
+const redirectPath = sessionStorage.getItem('redirectPath');
+if (redirectPath) {
+  sessionStorage.removeItem('redirectPath');
+  window.history.replaceState(null, '', redirectPath);
+}
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+
